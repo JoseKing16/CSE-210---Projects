8c86ae68c1b073b1259c801d0469ee9ef6a591b8
JoseKing16 <joseantking@gmail.com>
2026-07-15 22:21:06 -0400

    Complatation of the Step 4

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        //Add the object job1 for the variable member _jobTitle in Job
+        Job job1 = new Job();
+        job1._jobTitle = "Sofware Engineer";
+        //Prins the result
+        Console.WriteLine(job1._jobTitle);
+        
+        //Add the object company1 for the variable member _company in Job
+        Job company1 = new Job();
+        company1._company = "Apple";
+        //Prins the result
+        Console.WriteLine(company1._company);
+
+        //Another way to add an object... The job2 object for the variable member _jobTitle in Job
+        Job job2 = new()
+        {
+            _jobTitle = "IT"
+        };
+        //Prins the result
+        Console.WriteLine(job2._jobTitle);
+
+
+        //Another way to add an object... The company2 object for the variable member _company in Job
+        Job company2 = new()
+        {
+            _company = "Samsung"
+        };
+        //Prins the result
+        Console.WriteLine(company2._company);
+
     }
 }
